087e03ae4e5ef28342b79f8535e9f0dbd57622ab
Andresito03 <barincon@alumnat.copernic.cat>
2024-11-09 21:49:02 +0100

    Primer cambio
    realice un cambio al archivo Prueba1.java por primera vez

--- Prueba1.java
+++ Prueba1.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Pruebas_git;
 
-/**
- *
- * @author pc
- */
 public class Prueba1 {
+    public static void main ( String[] args){
     
+        System.out.println("Hola");
+    }
 }
