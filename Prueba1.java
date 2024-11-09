e737499fb233c76454311aec5d3b22145e8c471e
Andresito03 <barincon@alumnat.copernic.cat>
2024-11-09 21:49:37 +0100

    Segundo cambio
    Realice un cambio... bueno eso

--- Prueba1.java
+++ Prueba1.java
@@ -4,6 +4,6 @@ package Pruebas_git;
 public class Prueba1 {
     public static void main ( String[] args){
     
-        System.out.println("Hola");
+        System.out.println("Hola, segundo cambio");
     }
 }
