a00a55c1a97c15c8130d3028d22296af5b5c93c1
Andresito03 <barincon@alumnat.copernic.cat>
2024-11-09 21:37:31 +0100

    Add files via upload
    Subo los ficheros

---
+++ Prueba2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Pruebas_git;
+
+/**
+ *
+ * @author pc
+ */
+public class Prueba2 {
+    
+}
